86f091ccad4263f5bd2a40255593be89eab5cfbc
Mualen Baada Jerry <kwesijerry56@gmail.com>
2023-05-31 15:19:08 -0600

    Changed the text color to white and increased the size to 50px

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,16 @@ void main() {
             Color.fromARGB(255, 26, 2, 80),
             Color.fromARGB(255, 45, 7, 98),
             Color.fromARGB(255, 12, 1, 36),
-          ]),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          ),
         ),
         child: const Center(
-          child: Text('Hello World !'),
+          child: Text('Hello World !', style: TextStyle(
+            fontSize: 50,
+            color: Colors.white,
+          ), ), 
         ),
       ),
     )),
