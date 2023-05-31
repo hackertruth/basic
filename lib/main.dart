de770a7e4ffaf88c650c5267ec7dcb7266831c11
Mualen Baada Jerry <kwesijerry56@gmail.com>
2023-05-31 14:08:55 -0600

    Changed the backgorund color to a gradient color

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,20 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Scaffold(
-        backgroundColor: Color.fromARGB(255, 93, 38, 189),
-        body: Center(child: Text('Hello World !'),),
-      )
-    ),
+    MaterialApp(
+        home: Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(colors: [
+            Color.fromARGB(255, 26, 2, 80),
+            Color.fromARGB(255, 45, 7, 98),
+            Color.fromARGB(255, 12, 1, 36),
+          ]),
+        ),
+        child: const Center(
+          child: Text('Hello World !'),
+        ),
+      ),
+    )),
   );
 }
